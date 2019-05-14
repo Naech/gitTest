e37d61f5362f3830a70132c212787bf69cd6d02b
Naech <empnaechark@gmail.com>
2019-05-14 14:56:42 +0100

    first commit

---
+++ Book.java
@@ -0,0 +1,10 @@
+public class Book{
+
+    private String Author;
+    private int Pages;
+    private boolean Hardcover;
+    private String Publisher;
+
+    public static void main(String[] args){
+    }
+}
